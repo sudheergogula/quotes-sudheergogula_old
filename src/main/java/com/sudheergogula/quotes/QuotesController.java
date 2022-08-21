c07815409b7edb693bc6a7161a624cd793958abc
Sudheer Gogula (U108842) <sudheer.gogula.sp@lhsystems.com>
2022-08-21 17:11:52 +0530

    Version change

--- src/main/java/com/sudheergogula/quotes/QuotesController.java
+++ src/main/java/com/sudheergogula/quotes/QuotesController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Random;
 
 @Controller
 @RequiredArgsConstructor
